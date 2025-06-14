f42368b0e75c8349bee88a3322fe8f87f8378afa
ajinkya-cell <ajinkyaadharkar@gmail.com>
2025-06-14 14:32:05 +0530

    first commit

---
+++ src/hooks/use-geolocation.ts
@@ -0,0 +1,75 @@
+import type { Coordinates } from "@/api/types"
+import { useEffect, useState } from "react";
+
+
+interface GeolocationState{
+    coordinates : Coordinates | null;
+    error :string|null;
+    isLoading:boolean;
+}
+
+export function useGeolocation(){
+    const [locationData , setlocationData] = useState<GeolocationState>({
+        coordinates:null,
+        error:null,
+        isLoading:true
+    })
+
+    const getLocation=()=>{
+        setlocationData((prev)=>({...prev , isLoading:true , error:null}))
+
+        if(!navigator.geolocation){
+            setlocationData({
+                coordinates:null,
+                error:"Geolocation is not supported by your browser",
+                isLoading:false,
+            })
+            return
+        }
+        navigator.geolocation.getCurrentPosition((position)=>{
+            setlocationData({
+                coordinates:{
+                    lat:position.coords.latitude,
+                    lon:position.coords.longitude
+                },
+                error:null,
+                isLoading:false
+            })
+        },(error)=>{
+            let errorMessage:string;
+
+            switch(error.code){
+                case error.PERMISSION_DENIED:
+                    errorMessage="Location permission denied.Please enable location access."
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    errorMessage="Locaiton information is unavailable"
+                    break;
+                case error.TIMEOUT:
+                    errorMessage="Location request timed out"
+                    break;
+                default:
+                    errorMessage="An unknown error occured"   
+            }
+
+            setlocationData({
+                coordinates:null,
+                error:errorMessage,
+                isLoading:false
+            })
+        },{
+            enableHighAccuracy:true,
+            timeout:5000,
+            
+        })
+    }
+
+    useEffect(()=>{
+        getLocation();
+    },[])
+
+    return {
+        ...locationData,
+        getLocation
+    }
+}
